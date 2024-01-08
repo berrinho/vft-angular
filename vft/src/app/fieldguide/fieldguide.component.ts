fe1165c119fd8b40e688c180c73b3c66d6225922
Joe Berry <joe.berry@openathens.net>
2024-01-08 08:42:37 +0000

    Splits species by kingdom

--- vft/src/app/fieldguide/fieldguide.component.ts
+++ vft/src/app/fieldguide/fieldguide.component.ts
@@ -13,19 +13,41 @@ import { CommonModule } from '@angular/common';
 })
 export class FieldguideComponent implements OnInit, OnDestroy{
 
+
   speciesList!: Species[];
+  animalList!: Species[];
+  otherSpeciesList!: Species[];
   sub! : Subscription;
 
 constructor(private fieldguideService: FieldguideService){}
 
   ngOnInit(): void {
-    this.sub=this.fieldguideService.getSpeciesList().subscribe({
-      next: species => this.speciesList = species,
-    })
+    this.sub=this.fieldguideService.getSpeciesList().subscribe(
+      {
+        next: species => 
+        { 
+          this.speciesList = species;
+          this.animalList = [];
+          this.otherSpeciesList = [];
+          species.forEach(
+              e => { if (e.kingdom=='Animalia'){
+                  this.animalList.push(e);
+                }
+                else {
+                  this.otherSpeciesList.push(e);
+                }
+              }
+          )
+        }
+      }  
+    )
   }
 
   ngOnDestroy(): void {
     this.sub.unsubscribe();
   }
 
+  public getBase64(arg0: string): String {
+    return "data:image/png;base64,"+arg0;
+    }
 }
