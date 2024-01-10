9e75d0e2e4cb9e922e3623ea4165f1b0ab27215e
Joe Berry <joe.berry@openathens.net>
2024-01-10 16:03:47 +0000

    adds async pipe in fieldsite page and resizes images

--- vft/src/app/fieldsite/fieldsite.component.ts
+++ vft/src/app/fieldsite/fieldsite.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { Fieldsite } from './fieldsite';
-import { Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { FieldsiteService } from './fieldsite.service';
 import { Fieldtrip } from '../fieldtrip/fieldtrip';
-import { FieldtripService } from '../fieldtrip/fieldtrip.service';
 
 @Component({
   selector: 'app-fieldsite',
@@ -14,12 +13,11 @@ import { FieldtripService } from '../fieldtrip/fieldtrip.service';
   templateUrl: './fieldsite.component.html',
   styleUrl: './fieldsite.component.css'
 })
-export class FieldsiteComponent implements OnInit, OnDestroy {
-  currentSite!: Fieldsite;
-  currentTrip!: Fieldtrip;
+export class FieldsiteComponent implements OnInit {
+  currentSite$: Observable<Fieldsite> | undefined;
+  currentTrip$: Observable<Fieldtrip> | undefined;
   errorMessage: string="";
-  sub!: Subscription;
-  tripSub!: Subscription;
+
 
   constructor(private siteService: FieldsiteService, private activatedRoute: ActivatedRoute){
 
@@ -28,22 +26,7 @@ export class FieldsiteComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     var siteId = Number(this.activatedRoute.snapshot.paramMap.get("id"));
 
-    this.sub = this.siteService.getSite(siteId).subscribe({
-      next: site => {
-        this.currentSite = site;
-      },
-      error: err => this.errorMessage = err}
-    );
-
-    this.tripSub = this.siteService.getTripForSite(siteId).subscribe({
-      next: trip => {
-        this.currentTrip = trip;
-      },
-      error: err => this.errorMessage += err}
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.sub.unsubscribe();
+    this.currentSite$ = this.siteService.getSite(siteId);
+    this.currentTrip$ = this.siteService.getTripForSite(siteId);
   }
 }
